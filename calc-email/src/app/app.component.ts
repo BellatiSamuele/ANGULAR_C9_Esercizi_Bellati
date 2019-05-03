12467ba5fb9b756eb371fc254b0aa7b05029ca26
BellatiS <bellati.samuele@iisgalvanimi.edu.it>
2019-05-03 06:28:35 +0000

    input

--- calc-email/src/app/app.component.ts
+++ calc-email/src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc-email';
+
+  tot: number = 0;
+  numero: number = 0;
+  i: number = 0;
+
+
+  somma(tot: HTMLInputElement){
+    let totInt = Number(tot.value);
+    this.numero = this.numero + totInt;
+    this.i++;
+    console.log(this.numero);
+    return false;
+  }
+
+  sottrazione(tot: HTMLInputElement){
+    let totInt = Number(tot.value);
+    this.numero = this.numero - totInt;
+    this.i++;
+    console.log(this.numero);
+    return false;
+  }
+
+  moltiplicaz(tot: HTMLInputElement){
+    let totInt = Number(tot.value);
+    this.numero = this.numero * totInt;
+    this.i++;
+    console.log(this.numero);
+    return false;
+  }
+
+  divisione(tot: HTMLInputElement){
+    let totInt = Number(tot.value);
+    this.numero = this.numero / totInt;
+    this.i++;
+    console.log(this.numero);
+    return false;
+  }
+
 }
