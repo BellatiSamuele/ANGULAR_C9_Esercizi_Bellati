e8b731d175144291be2d60f2af9e3a1fbcde9314
BellatiS <bellati.samuele@iisgalvanimi.edu.it>
2019-05-03 07:32:08 +0000

    bootsrap

--- calc-email/src/app/app.module.ts
+++ calc-email/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { RisComponent } from './ris/ris.component';
@@ -10,7 +11,7 @@ import { RisComponent } from './ris/ris.component';
     RisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
